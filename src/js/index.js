dca65ab533071db1ef20f30c8e0301fc048fcc17
telesyk <telesyk89@gmail.com>
2025-05-13 14:47:35 +0200

    Update search-box naming, add Search and Tabs styles.
    Add JS functionality to catch focus on inputs.
    Update Buttons and Table styles.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 console.log('👻 index.js file loaded')
+/* Get Focus on inputs */
+const inputs = document.querySelectorAll('input')
+inputs.forEach(input => {
+  input.addEventListener('focus', event => {
+    handleInputFocus(event)
+  })
+  input.addEventListener('blur', event => {
+    handleInputFocus(event, true)
+  })
+})
+
+function handleInputFocus(event, isFocused = false) {
+  const { target } = event
+  if (isFocused) {
+    target.closest('label').classList.remove('is-focused')
+    return
+  }
+  target.closest('label').classList.add('is-focused')
+}
